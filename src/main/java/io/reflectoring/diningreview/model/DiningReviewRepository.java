19acb26167fb246a8b391aa15f5c57df42ec6abc
Lasse G <90897086+LasseMG@users.noreply.github.com>
2025-02-11 16:12:20 +0100

    Empty repositories added

---
+++ src/main/java/io/reflectoring/diningreview/model/DiningReviewRepository.java
@@ -0,0 +1,6 @@
+package io.reflectoring.diningreview.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface DiningReviewRepository extends CrudRepository<DiningReview, Long> {
+}
---
+++ src/main/java/io/reflectoring/diningreview/model/RestaurantRepository.java
@@ -0,0 +1,6 @@
+package io.reflectoring.diningreview.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface RestaurantRepository extends CrudRepository<Restaurant, Long> {
+}
